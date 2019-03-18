b3744bf583069a7d4df31199a84c9c64256bd2a2
devesh166 <jiodevesh@.com>
2019-03-18 14:32:21 +0530

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilterComponent } from './filter/filter.component';
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilterComponent
+    FilterComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -6,11 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
-
+  categoryflag :boolean=true;
+  sizeflag :boolean=true;
   constructor() { }
 
   ngOnInit() {
     
   }
+  categoriesFunction(){
+    let temp = document.getElementById("categoriesbox");
+    if(this.categoryflag){
+      temp.style.display="block";
+      document.getElementById("cat").innerHTML="- Categories"
+      
+    console.log("inn")
+    this.categoryflag =false;
+    }else{
+      temp.style.display="none";
+      this.categoryflag =true;
+      document.getElementById("cat").innerHTML="+ Categories"  }
+    
 
+  }
+  sizeFunction(){
+    let temp = document.getElementById("sizebox");
+    if(this.sizeflag){
+      temp.style.display="block";
+      document.getElementById("siz").innerHTML="- Size"
+      
+    console.log("inn")
+    this.sizeflag =false;
+    }else{
+      temp.style.display="none";
+      this.sizeflag =true;
+      document.getElementById("siz").innerHTML="+ Size"    }
+    
+
+  }
 }
